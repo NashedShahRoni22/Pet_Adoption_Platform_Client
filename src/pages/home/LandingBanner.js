47468d0efc79ddfd903bacbb7186367b7c71378e
Creativecoder11 <creativecoderpro@gmail.com>
2023-04-27 12:56:42 +0600

    Small UI Fix in Home Page

--- src/pages/home/LandingBanner.js
+++ src/pages/home/LandingBanner.js
@@ -7,9 +7,9 @@ const LandingBanner = () => {
     height: 500,
   };
   return (
-    <section className="py-10 mx-5 md:flex items-center justify-center h-[60vh]">
+    <section className="mx-5 md:flex items-center justify-center h-[60vh]">
       <div className="md:w-1/2">
-        <h1 className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-500 to-pink-500  text-6xl lg:text-8xl font-extrabold">
+        <h1 className="mt-10 md:mt-0 text-transparent bg-clip-text bg-gradient-to-tr from-blue-500 to-pink-500  text-4xl md:text-6xl lg:text-8xl font-extrabold">
           Find the Right <br /> Pet For You
         </h1>
         <p className="mt-5 md:text-xl md:font-semibold text-justify">
